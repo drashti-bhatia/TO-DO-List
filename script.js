c13c5393da0c2182136d26b39704bbf0d333c0f4
drashti-bhatia <bhatiagrishti@gmail.com>
2025-02-20 15:49:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function addContent() {
+    // let card = `<div><textarea name="content1" id="content1" rows="25" cols="45"></textarea></div>`;
+    // let card = `<div><textarea name="content1" id="content1" rows="25" cols="45"></textarea></div>`;
+
+    let card = `<div class="card">
+                    <textarea class="card-header" placeholder="Heading.."></textarea>
+                    <textarea class="card-textarea" placeholder="Write your task here..."></textarea>
+                    <button class="btn" onclick="saveTask(this)">Save</button>
+                    <button class="btn" onclick="deleteTask(this)">Delete</button>
+                </div>`;
+
+    document.getElementById("content").innerHTML += card;
+}
