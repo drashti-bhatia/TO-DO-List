a639f512faa7d45b00ab5ad2038d105557a82583
drashti-bhatia <bhatiagrishti@gmail.com>
2025-02-20 16:08:14 +0530

    styling save and delete button

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function addContent() {
     let card = `<div class="card">
                     <textarea class="card-header" placeholder="Heading.."></textarea>
                     <textarea class="card-textarea" placeholder="Write your task here..."></textarea>
-                    <button class="btn" onclick="saveTask(this)">Save</button>
-                    <button class="btn" onclick="deleteTask(this)">Delete</button>
+                    <div class="position-btn">
+                        <button class="btn" onclick="saveTask(this)">Save</button>
+                        <button class="btn" onclick="deleteTask(this)">Delete</button>
+                    </div>
                 </div>`;
 
     document.getElementById("content").innerHTML += card;
